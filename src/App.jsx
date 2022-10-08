021b336c47b363924bb1e2d758c7508d95732194
Abraham Rivera <trarzep@gmail.com>
2022-10-08 13:02:18 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './routes/home/home.jsx'
+import './App.css'
+
+function App() 
+{
+  
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/routes/home/home.jsx
@@ -0,0 +1,10 @@
+function Home()
+{
+    return( 
+    <div className="homeContainer">
+        <h1 className="homeTitle">Home</h1>
+        <p>He intentado aplicaciones de tareas y no me funcionan, no puedo organizarme</p>
+    </div>);
+}
+
+export default Home;
